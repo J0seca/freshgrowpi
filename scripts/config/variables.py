cc17d0bae674719ff682719bcff49b971881733e
j0seca <carlosabarcav@gmail.com>
2021-05-09 15:34:57 -0400

    unos cuantos bugs menos

---
+++ scripts/config/variables.py
@@ -0,0 +1,8 @@
+vent_temp_max = 21
+vent_hum_max = 80
+ext_temp_max = 23
+ext_hum_max = 90
+luz_hora_encendido = "10:00"
+luz_hora_apagado = "23:00"
+correo_datos = "carlos@freshgrowpi.cl"
+frecuencia_correos = "12"
